c68990b257af5054b90895342adc9ff9fbade305
fas <facundosierrar@gmail.com>
2023-02-16 01:28:22 +0100

    done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::io::{self, Write};
 
 /// Search for a pettern in a file and display the lines that cointain it.
 #[derive(Parser)]
@@ -11,14 +12,16 @@ struct Cli {
 
 
 fn main() {
+    let stdout = io::stdout(); // get the global stdout entity
+    let mut handle = io::BufWriter::new(stdout);
+
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    
+    let content = std::fs::read_to_string(&args.path).unwrap();
+
+
     for line in content.lines() {
         if line.contains(&args.pattern) {
-            println!("{}", line);
+            writeln!(handle, "{}", line);
         }
     }
-
-    dbg!("done!");
 }
